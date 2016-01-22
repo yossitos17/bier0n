bf1232d71b5481616e0d51730379ccb104a5932a
usuario <usuario@usuario-ALDA>
2016-01-22 12:42:35 +0100

    Primera versión del bier0n

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+Bier0n es una aplicación web sobre cerveza.
+Copyright (C) 2016  José María Rodríguez Toledo.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bier0n | Versión de pruebas</title>
+        <link rel="stylesheet" href="./css/estilos.css" type="text/css"/>
+        <script type="text/javascript" src="./js/javascript.js"></script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
