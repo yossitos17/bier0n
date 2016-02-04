8058d5893f1f0e3dd9d15057116b571c1c9a7f72
usuario <usuario@usuario-ALDA>
2016-02-04 13:03:02 +0100

    Install que funciona (increibla)

--- install/install.php
+++ install/install.php
@@ -30,16 +30,16 @@ and open the template in the editor.
                 mysqli_query($conex, $sql) or die("Error al utilizar la base de datos.");
 
                  //1 Consulta SQL que crea la tabla de usuarios.
-                $sql = "CREATE TABLE if not exists `bieron`.`usuarios` (
-                `idusuario` INT(15) NOT NULL AUTO_INCREMENT,
-                `login` VARCHAR(20) NOT NULL,
-                `password` VARCHAR(300) NOT NULL,
-                `edad` INT (2) NOT NULL,
-                `sexo` SET('Hombre','Mujer') NOT NULL,
-                `rol` SET('Administrador', 'Registrado') NOT NULL default 'Registrado',
-                PRIMARY KEY (`idusuario`)
-            )   ENGINE=InnoDB;";
-                mysqli_query($conex, $sql) or die("Error al crear la tabla usuarios.");
+                $sql = "CREATE TABLE if not exists usuarios (
+                idusuarios INT(15) NOT NULL AUTO_INCREMENT,
+                login VARCHAR(20) NOT NULL,
+                password VARCHAR(300) NOT NULL,
+                edad INT(2) NOT NULL,
+                sexo SET('Hombre','Mujer') NOT NULL,
+                rol SET('Administrador','Registrado') NOT NULL default 'Registrado',
+                PRIMARY KEY (idusuarios)
+            )  ENGINE=InnoDB;";
+                mysqli_query($conex, $sql) or die("Error al crear la tabla usuarios.".mysqli_error($conex));
                 
                 echo "OK.<br>Creando la tabla 'cervezas'...";
                 
@@ -47,47 +47,47 @@ and open the template in the editor.
               
                
                  //2 Consulta SQL que crea la tabla de cervezas.
-                $sql = "CREATE TABLE if not exists `cervezas´ (
-                `idcerve` INT(15) NOT NULL AUTO_INCREMENT,
-                `nombre` VARCHAR(30) NOT NULL,
-                `modelo` VARCHAR (40) NOT NULL,
-               `graduacion` INT(3) NOT NULL,
-               `imagen` VARCHAR(2) NOT NULL,
-                 PRIMARY KEY (`idcerve`)
+                $sql = "CREATE TABLE if not exists cervezas (
+                idcervezas INT(15) NOT NULL AUTO_INCREMENT,
+                nombre VARCHAR(30) NOT NULL,
+                modelo VARCHAR (40) NOT NULL,
+                graduacion INT(3) NOT NULL,
+                imagen VARCHAR(2) NOT NULL,
+                 PRIMARY KEY (idcervezas)
                )  ENGINE=InnoDB;";    
-               mysqli_query($conex, $sql) or die("Error al crear la tabla cervezas.");
+               mysqli_query($conex, $sql) or die("Error al crear la tabla cervezas.".mysqli_error($conex));
                
                 echo "OK.<br>Creando la tabla 'establecimientos'...  ";
                 
                  mysqli_query($conex, $sql) or die("Error al utilizar la base de datos.");
                  
                   //3 Consulta SQL que crea la tabla de establecimientos.
-                $sql = "CREATE TABLE if not exists `bieron`.`establecimientos` (
-                `idestablecimiento` INT(20) NOT NULL AUTO_INCREMENT,
-                `dirección` VARCHAR(30) NOT NULL,
-                `ciudad´ VARCHAR(30),
-                `precio` INT(40) NOT NULL,
-                `nombreestablecimiento´ INT (40) NOT NULL,
-                PRIMARY KEY (`idestablecimiento`)
+                $sql = "CREATE TABLE if not exists establecimientos (
+                idestablecimientos INT(20) NOT NULL AUTO_INCREMENT,
+                dirección VARCHAR(30) NOT NULL,
+                ciudad VARCHAR(30),
+                precio INT(40) NOT NULL,
+                nombreestablecimiento INT (40) NOT NULL,
+                PRIMARY KEY (idestablecimientos)
                   )  ENGINE=InnoDB;";    
-                mysqli_query($conex, $sql) or die("Error al crear la tabla establecimientos.");
+                mysqli_query($conex, $sql) or die("Error al crear la tabla establecimientos:.".mysqli_error($conex));
                
                 echo "OK.<br>Creando la tabla 'valoración'...  ";
                 
                  mysqli_query($conex, $sql) or die("Error al utilizar la base de datos.");
                  
                    //4 Consulta SQL que crea la tabla de valoración.
-                $sql = "CREATE TABLE if not exists `bieron`.`valoración` (
-                `idcervezas` INT(20) NOT NULL AUTO_INCREMENT,
-                `idusuario` INT(15) NOT NULL AUTO_INCREMENT,
-                `puntuación´ INT(2) NO NULL,
-                `comentario` VARCHAR(400) NOT NULL,
-                `fechasubida` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                FOREING KEY (`idcervezas´) REFERENCES cervezas(idcervezas),
-                FOREING KEY (`idsuarios´) REFERENCES usuarios(idusuarios),
-                PRIMARY KEY (`idcerveza`,`idusuarios´)
+                $sql = "CREATE TABLE if not exists valoración (
+                idcervezas INT(20) NOT NULL,
+                idusuarios INT(15) NOT NULL,
+                puntuación INT(2) NOT NULL,
+                comentario VARCHAR(350) NOT NULL,
+                fechasubida TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (idcervezas) REFERENCES cervezas(idcervezas),
+                FOREIGN KEY (idusuarios) REFERENCES usuarios(idusuarios),
+                PRIMARY KEY (idcervezas, idusuarios)
                   )  ENGINE=InnoDB;";    
-                mysqli_query($conex, $sql) or die("Error al crear la tabla valoración.");
+                mysqli_query($conex, $sql) or die("Error al crear la tabla valoración.".mysqli_error($conex));
                
                 echo "OK.<br>Creando la tabla 'cervezasestablecimientos´...  ";
                 
@@ -95,13 +95,13 @@ and open the template in the editor.
                  
                    //5 Consulta SQL que crea la tabla de cervezasestablecimientos.
                 $sql = "CREATE TABLE if not exists `bieron`.`cervezasestablecimientos` (
-                `idcervezas` INT(20) NOT NULL AUTO_INCREMENT,
-                `idestablecimiento` INT(15) NOT NULL AUTO_INCREMENT,
-                FOREING KEY (`idcervezas´) REFERENCES cervezas(idcervezas),
-                FOREING KEY (`idestablecimientos´) REFERENCES establecimientos(idestablecimientos),
-                PRIMARY KEY (`idcerveza`,`idestablecimiento´)
+                idcervezas INT(20) NOT NULL,
+                idestablecimientos INT(15) NOT NULL,
+                FOREIGN KEY (idcervezas) REFERENCES cervezas(idcervezas),
+                FOREIGN KEY (idestablecimientos) REFERENCES establecimientos(idestablecimientos),
+                PRIMARY KEY (idcervezas, idestablecimientos)
                   )  ENGINE=InnoDB;";    
-                mysqli_query($conex, $sql) or die("Error al crear la tabla cervezasestablecimientos.");
+                mysqli_query($conex, $sql) or die("Error al crear la tabla cervezasestablecimientos.".mysqli_error($conex));
                
                 echo "OK.<br>`SUCCES´  ";
                 
